fa51919a27951481044d90b7beac26a8fcca901a
dnksebastian <sebastian.dulniawka@gmail.com>
2024-03-03 18:51:41 +0100

    django blog

--- django-blog/djangonautic/articles/models.py
+++ django-blog/djangonautic/articles/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+# TIP: commands
+# python manage.py makemigrations
+# python manage.py migrate
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    #add in thumbnail later
+    #add author later
