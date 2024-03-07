574176880433692230d7be87632e0a3abc420c61
dnksebastian <sebastian.dulniawka@gmail.com>
2024-03-07 19:52:31 +0100

    django blog

--- django-blog/djangonautic/articles/urls.py
+++ django-blog/djangonautic/articles/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.article_list)
+    path('', views.article_list),
+    path('<slug:slug>/', views.article_detail), #re_path(r'^(?P<slug>[\w-]+)/$', views.article_detail),
+    
 ]
