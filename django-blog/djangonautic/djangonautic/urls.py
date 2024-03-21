ca28141270e016458fae529294c1197c0d2ce415
dnksebastian <sebastian.dulniawka@gmail.com>
2024-03-21 21:19:40 +0100

    django blog

--- django-blog/djangonautic/djangonautic/urls.py
+++ django-blog/djangonautic/djangonautic/urls.py
@@ -20,13 +20,15 @@ from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
+from articles import views as article_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('articles/', include('articles.urls')),
     path('about', views.about),
-    path('', views.homepage)
+    path('', article_views.article_list, name='home')
+    # path('', views.homepage)
     # path(r'^about/$', views.about),
     # path(r'^$', views.homepage),
 ]
